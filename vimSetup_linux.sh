c70b62b634f80889494e9994c98125ff63210fe7
Mecu Mihai Sorin <mecusorin@gmail.com>
2015-08-12 20:33:37 +0300

    Updated the linux script with a hook to remove easy the vim plugins install

--- vimSetup_linux.sh
+++ vimSetup_linux.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+#direct acces by url: https://raw.githubusercontent.com/MecuSorin/vim/master/vimSetup_linux.sh
 
 mkdir -p ~/.vim ~/.vim/undoHistory ~/.vim/autoload ~/.vim/bundle ~/.vim/old_vimrc
 git clone https://github.com/MecuSorin/vim.git ~/.vim/todelete
@@ -8,12 +9,17 @@ cp -r ~/.vim/todelete/startup ~/.vim
 rm -rf ~/.vim/todelete
 dos2unix ~/.vim/startup/*
 
-curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
-cd ~/.vim/bundle
-git clone https://github.com/kien/ctrlp.vim.git
-git clone https://github.com/scrooloose/nerdcommenter.git
-git clone https://github.com/scrooloose/nerdtree.git
-git clone https://github.com/scrooloose/syntastic.git
-git clone https://github.com/bling/vim-airline
-git clone git://github.com/altercation/vim-colors-solarized.git
-git clone https://github.com/fatih/vim-go.git
+
+# Install plugins, if is used with sed to disable this step
+if [ 1 -ne 0 ] 
+	then
+		curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim	
+		cd ~/.vim/bundle
+		git clone https://github.com/kien/ctrlp.vim.git
+		git clone https://github.com/scrooloose/nerdcommenter.git
+		git clone https://github.com/scrooloose/nerdtree.git
+		git clone https://github.com/scrooloose/syntastic.git
+		git clone https://github.com/bling/vim-airline
+		git clone git://github.com/altercation/vim-colors-solarized.git
+		git clone https://github.com/fatih/vim-go.git
+fi
